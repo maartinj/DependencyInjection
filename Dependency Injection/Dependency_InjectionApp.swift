59fe3043b8b56e14274a1b30cc9a56ad1c1e353d
Marcin Jędrzejak <53284922+maartinj@users.noreply.github.com>
2023-07-14 16:52:34 +0200

    first commit: Load initial files

---
+++ Dependency Injection/Dependency_InjectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dependency_InjectionApp.swift
+//  Dependency Injection
+//
+//  Created by Marcin Jędrzejak on 14/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct Dependency_InjectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
